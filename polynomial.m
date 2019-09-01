2b62948f68a0f04d43fb3028607a4d567a85c7ee
jqjq <52230035+jqjq@users.noreply.github.com>
2019-09-01 21:25:10 +0200

    update

--- polynomial.m
+++
@@ -1,6 +0,0 @@
-function y = polynomial(a, x)
-    y = 0;
-    for i = length(a)-1:0
-        y = a(i+1) + x * y;
-    end
-end
--- polynomialx.m
+++
@@ -1,6 +0,0 @@
-function y = polynomialx(a, x)
-    y = 0;
-    for i = 0:length(a)-1
-        y = y + a(i+1) * x ^ i;
-    end
-end

29c73b9cb9d5dc2462299a45b70d81d71362bfe9
jqjq <52230035+jqjq@users.noreply.github.com>
2019-09-02 13:59:49 +0200

    update

--- +rndm/randbit.m
+++ +rndm/randbit.m
@@ -1,20 +1,20 @@
-function ret = randbit()
+function bit = randbit()
     while true
         a = biased_randbit();
         b = biased_randbit();
         if a > b
-            ret = 0;
+            bit = 0;
             return
         elseif a < b
-            ret = 1;
+            bit = 1;
             return
         end
     end
 end
 
-function ret = biased_randbit()
-    ret = randi([1, 3]);
-    if ret ~= 1
-        ret = 0;
+function bit = biased_randbit()
+    bit = randi([1, 3]);
+    if bit ~= 1
+        bit = 0;
     end
 end

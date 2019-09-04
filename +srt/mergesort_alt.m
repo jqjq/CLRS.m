8759746f9445a805b3363c3673b948475c38eebc
jqjq <52230035+jqjq@users.noreply.github.com>
2019-09-04 20:42:53 +0200

    update

--- +srt/mergesort_alt.m
+++ +srt/mergesort_alt.m
@@ -1,29 +1,23 @@
-function [total_inv, a] = inversion(a, p, r)
-    total_inv = 0;
+function a = mergesort_alt(a, p, r)
     if p < r
         q = fix((p + r)/2);
-        [inv, a] = srt.inversion(a, p, q);
-        total_inv = total_inv + inv;
-        [inv, a] = srt.inversion(a, q+1, r);
-        total_inv = total_inv + inv;
-        [inv, a] = merge(a, p, q, r);
-        total_inv = total_inv + inv;
+        a = srt.mergesort_alt(a, p, q);
+        a = srt.mergesort_alt(a, q+1, r);
+        a = merge(a, p, q, r);
     end
 end
 
-function [inv, a] = merge(a, p, q, r)
+function a = merge(a, p, q, r)
     left = a(p:q);
     right = a(q+1:r);
     i = 1;
     j = 1;
-    inv = 0;
     try
         for k = p:r
             if left(i) <= right(j)
                 a(k) = left(i);
                 i = i + 1;
             else
-                inv = inv + length(left) - i + 1;
                 a(k) = right(j);
                 j = j + 1;
             end

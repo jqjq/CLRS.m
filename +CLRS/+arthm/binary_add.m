0866bd95ccb87bf65591b3d40fafa47e3b10e5d9
jqjq <52230035+jqjq@users.noreply.github.com>
2020-01-12 17:11:49 +0800

    Delete binary_add.m

--- +CLRS/+arthm/binary_add.m
+++
@@ -1,9 +0,0 @@
-function c = binary_add(a, b)
-    carry = 0;
-    for i = length(a):-1:1
-        sum = a(i) + b(i) + carry;
-        c(i+1) = rem(sum, 2);
-        carry = sum >= 2;
-    end
-    c(1) = carry;
-end
